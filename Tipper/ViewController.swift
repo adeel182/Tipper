3beffb4ee4cb0d253c5c629d4416548a806a96a7
Adeel Bhatti <adeel182@gmail.com>
2019-09-28 14:24:53 -0700

    Added functionality for calculating tips

--- Tipper/ViewController.swift
+++ Tipper/ViewController.swift
@@ -9,12 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+   
+    @IBOutlet weak var BilLField: UITextField!
+    @IBOutlet weak var TipLabel: UILabel!
+    @IBOutlet weak var TotalLabel: UILabel!
+    
+    @IBOutlet weak var percentageLabelf: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    @IBAction func onTap(_ sender: Any) {
+       
+        view.endEditing(true)
+    }
+    @IBAction func TipCalc(_ sender: Any) {
+        
+        let bill = Double(BilLField.text!) ?? 0
+        
+//        get the bill amount
+        let percentages = [0.1, 0.15, 0.2]
+        let tip = bill * percentages[percentageLabelf.selectedSegmentIndex]
+        let total = bill + tip
+        
+        TipLabel.text = String(format: "$%.2f",tip)
+        TotalLabel.text = String(format: "$%.2f",total)
+//         calc tip total
+//update tip and total labels
+    }
 }
 
